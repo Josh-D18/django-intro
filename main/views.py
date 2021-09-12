6dd83b03e514610bff7844a497dd860f3dc01e92
unknown <jjdate@live.com>
2021-09-12 16:54:42 -0400

    first commit

--- main/views.py
+++ main/views.py
@@ -12,7 +12,9 @@ def index(request):
   context = {
     'results': [
       {'name': 'Fatima Lopez', 'email': 'f.lopez@email.com'},
-      {'name': 'Gary Johnston', 'email': 'g.johnston@email.com'}
+      {'name': 'Gary Johnston', 'email': 'g.johnston@email.com'},
+      {'name': 'Mari Gotze', 'email': 'mario.gotze23@gmail.com'},
+      {'name': 'Gary Johnston', 'email': 'omar.ysuf12@yahoo.com'}
     ]
   }
   return render(request, 'index.html', context)
